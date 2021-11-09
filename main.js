046c41ed642c92e561c2c251fe127ff6aebaa436
PaoloPascucci <pascuccip9@gmail.com>
2021-11-09 16:37:28 +0100

    Add ciclo for

--- main.js
+++ main.js
@@ -2,15 +2,17 @@ const app = new Vue({
     el: '#app',
     data: {
         emails: [],
-        mounted() {
-            for (let i = 0; i < 10; i++) {
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-                    .then(response => {
-                        this.emails = response.data.response
-                    });
-            }
-        }
-    }
+    },
+    mounted() {
 
+        for (let i = 0; i < 10; i++) {
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(rispy => {
+                    let answer = rispy.data.response
+                    this.emails.push(answer)
+                })
+        }
+        console.log(this.emails);
+    }
 })
-console.log(this.emails);
